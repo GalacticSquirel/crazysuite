902566e2c5448b2796b1e3085aa740938e7586cc
Owen <GalacticSquirrell@gmail.com>
2022-08-29 14:18:50 +0100

    Update main.py

--- web/main.py
+++ web/main.py
@@ -2,7 +2,24 @@ from flask import Flask , request, jsonify
 import os
 from git.cmd import Git
 from git.repo import Repo
+from flask_mysqldb import MySQL
 app = Flask(__name__)
+
+# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+#     username="csuite",
+#     password="ez37nWS6Z6DfsrA",
+#     hostname="csuite.mysql.eu.pythonanywhere-services.com",
+#     databasename="the database name you chose, probably csuite$default",
+# )
+# app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+# app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['MYSQL_HOST'] = 'csuite.mysql.eu.pythonanywhere-services.com'
+app.config['MYSQL_USER'] = 'csuite'
+app.config['MYSQL_PASSWORD'] = 'ez37nWS6Z6DfsrA'
+app.config['MYSQL_DB'] = 'default'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
@@ -17,3 +34,14 @@ def webhook():
         return jsonify({"message" : str(repopull)})
     else:
         return jsonify({"error" : "Failed"})
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    name = request.headers['name']
+    age = request.headers['age']
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+    mysql.connection.commit()
+    cursor.close()
+    return f"{name,age} inserted"
+
