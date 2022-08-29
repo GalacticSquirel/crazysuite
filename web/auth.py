b0a1df1d8f4ef276e6231c2860fb56163c0ae7c8
Owen <GalacticSquirrell@gmail.com>
2022-08-29 19:13:31 +0100

    fixed profile

--- web/auth.py
+++ web/auth.py
@@ -29,7 +29,7 @@ def login(): # define login page fucntion
             return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
         # if the above check passes, then we know the user has the right credentials
         login_user(user, remember=remember)
-        return redirect(url_for('main.profile'))
+        return redirect(url_for('main.account'))
 
 @auth.route('/signup', methods=['GET', 'POST'])# we define the sign up path
 #@login_required
