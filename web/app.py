e64bc3b092f8d6c12adf085947aedbe654163fd1
Owen <GalacticSquirrell@gmail.com>
2022-08-30 04:23:04 +0100

    items are systematically generated and info passed through to productdetails.html

--- web/app.py
+++ web/app.py
@@ -34,9 +34,49 @@ def favicon():
 def account():
     return render_template('account.html', name=current_user.name)
 
+
+
 @main.route('/shop')
 def shop():
-    return render_template("shop.html")
+    def add_url(x):
+        x["url"] = url_for("main.productdetails", **x)
+        return x
+    shop_items = [{"name": "Product 1", "description": "description of product 1 be like","price": "£9.99","genre": "Software",
+                   "image_url": url_for('main.images', image_name='product1'), "big_image_url": url_for('main.images', image_name='product1')},
+                  {"name": "Product 2", "description": "description of product 2 be like something else","price": "£9.90","genre": "Software",
+                   "image_url": url_for('main.images', image_name='product1'), "big_image_url": url_for('main.images', image_name='product1')}, 
+                  {"name": "Product 3", "description": "description of product 3 be like something that isnt product 7","price": "£5.99","genre": "Software",
+                   "image_url": url_for('main.images', image_name='product1'), "big_image_url": url_for('main.images', image_name='product1')},
+                  {"name": "Product 4", "description": "description of product 4 be like something that isnt product 6","price": "£3.99","genre": "Software",
+                   "image_url": url_for('main.images', image_name='product1'), "big_image_url": url_for('main.images', image_name='product1')},
+                  {"name": "Product 5", "description": "description of product 5 be like something that isnt product 5","price": "£6.99","genre": "Software",
+                   "image_url": url_for('main.images', image_name='product1'), "big_image_url": url_for('main.images', image_name='product1')},
+                  {"name": "Product 6", "description": "description of product 6 be like something that isnt product 4","price": "£7.99","genre": "Software",
+                   "image_url": url_for('main.images', image_name='product1'), "big_image_url": url_for('main.images', image_name='product1')},
+                  {"name": "Product 7", "description": "description of product 7 be like something that isnt product 3","price": "£2.99","genre": "Software",
+                   "image_url": url_for('main.images', image_name='product1'), "big_image_url": url_for('main.images', image_name='product1')}]
+    with_urls = list(map(lambda x: add_url(x), shop_items))
+    return render_template("shop.html", shop_items=with_urls)
+
+@main.route("/productdetails/")
+def productdetails():
+
+    item_details = {"name": request.args.get('name', default = 1, type = str),
+                    "description": request.args.get('description', default = 1, type = str),
+                    "genre": request.args.get('genre', default = 1, type = str),
+                    "image_url": request.args.get('image_url', default = 1, type = str),
+                    "price": request.args.get('price', default = 1, type = str),
+                    "big_image_url": request.args.get('big_image_url', default = 1, type = str)}
+
+    return render_template("productdetails.html", item_details=item_details)
+
+@main.route("/productdetails/productdetails.css")
+def product_detailscss():
+    return send_file("templates//productdetails.css")
+
+@main.route("/productdetails/style.css")
+def product_detailsstylecss():
+    return send_file("templates//style.css")
 
 @main.route("/products")
 @login_required  
@@ -63,9 +103,12 @@ def productscss():
 def shopcss():
     return send_file("templates//shop.css")
 
-@main.route("/placehold_image")
-def placehold():
-    return send_file("templates//place_hold.png")
+@main.route("/images/<image_name>")
+def images(image_name):
+    if str(image_name) +".png" in os.listdir("templates//images"):
+        return send_file(f"templates//images//{image_name}.png")
+    else:
+        return send_file(f"templates//images//place_hold.png")
 
 app = create_app()
 
